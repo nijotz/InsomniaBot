fe8514ec68c7c3cef58c8f659f66e20a563b9181
Nick Tzaperas <nick@nijotz.com>
2014-04-27 02:08:52 -0700

    Added a pipe to rate limit streams

--- test_pipe.py
+++ test_pipe.py
@@ -1,11 +1,13 @@
+import re
 from socketproxy import Pipe, PlumbingServer, main
 import os
-from mutagen.mp3 import MP3
+import time
 
 
 class TestProxy(PlumbingServer):
     def __init__(self, *args, **kwargs):
         PlumbingServer.__init__(self, *args, **kwargs)
+        self.pipes.append(RateLimitPipe())
         self.pipes.append(AudioPipe('horn', open('airhorn.mp3')))
         self.pipes.append(AudioPipe('trumpet', open('trumpet.mp3')))
 
@@ -31,6 +33,33 @@ class AudioPipe(Pipe):
             else:
                 data = self.buff + data[:-len(self.buff)]
                 self.buff = ''
+
+        return data
+
+
+class RateLimitPipe(Pipe):
+
+    def __init__(self):
+        self.bitrate = 0
+        self.time_of_next_send = 0
+
+    def to_client(self, data):
+
+        sleep_time = self.time_of_next_send - time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+
+        if data.find('icy-br') != -1:
+            regex = re.compile('icy-br:(?P<bitrate>[0-9]*)')
+            match = regex.search(data)
+            if match:
+                bitrate = int(match.groupdict()['bitrate'])
+                self.bitrate = bitrate
+
+        if self.bitrate:
+            bits = len(data) * 8
+            secs_til_next_send = float(bits) / (self.bitrate * 1024)
+            self.time_of_next_send = time.time() + secs_til_next_send
 
         return data
 
